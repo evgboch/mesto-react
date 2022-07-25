f7a46097fff3782af9c5a31e1219e9bfcffd700a
Evgeny Bocharov <evgbocharov@gmail.com>
2022-07-25 22:07:29 +0300

    feat: создать компонент Api, реализовать с помощью него загрузку информации о пользователе

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,41 @@
+import React from "react";
+import { api } from "../utils/Api";
 import avatar from "../images/default-user.png";
 
 function Main(props) {
+  const [userDescription, setUserDescription] = React.useState();
+  const [userAvatar, setUserAvatar] = React.useState();
+  const [userName, setUserName] = React.useState();
+
+  React.useEffect(() => {
+    api.getUserInfo()
+      .then((data) => {
+        setUserName(data.name);
+        setUserDescription(data.about);
+        setUserAvatar(data.avatar);
+      })
+      .catch((err) => {
+        console.log(err);
+        setUserName("User");
+        setUserDescription("Info");
+        setUserAvatar(avatar);
+      });
+  });
+
   return(
     <main className="content">
       <section className="profile content__profile">
         <div className="profile__info">
           <div className="profile__avatar-container">
-            <img className="profile__avatar" src={avatar} alt="Образец аватара"/>
+            <div className="profile__avatar" style={{ backgroundImage: `url(${userAvatar})` }}/>
             <div className="profile__avatar-overlay" onClick={props.onEditAvatar}></div>
           </div>
           <div className="profile__text-container">
             <div className="profile__title-container">
-              <h1 className="profile__title">User</h1>
+              <h1 className="profile__title">{userName}</h1>
               <button className="profile__edit-button" onClick={props.onEditProfile} type="button"></button>
             </div>
-            <p className="profile__subtitle">Info</p>
+            <p className="profile__subtitle">{userDescription}</p>
           </div>
         </div>
         <button className="profile__add-button" onClick={props.onAddPlace} type="button"></button>
