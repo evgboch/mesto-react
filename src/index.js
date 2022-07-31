678deeb1bd36a39a7332803fe081446f360efbc3
Evgeny Bocharov <evgbocharov@gmail.com>
2022-07-31 17:14:36 +0300

    style: удалить лишние закомментированные блоки, картинки

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import "./index.css";
 import App from "./components/App.js";
 // import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById("root")
-// );
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
