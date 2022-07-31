ed942fefa029cd60352f4d0e21b79e6942a58e5f
Evgeny Bocharov <evgbocharov@gmail.com>
2022-07-31 17:05:15 +0300

    feat: добавить в App компонент с попапом подтверждения удаления карточки

--- src/components/App.js
+++ src/components/App.js
@@ -37,12 +37,14 @@ function App() {
   return (
     <div className="page__container">
       <Header />
+
       <Main
         onEditAvatar={handleEditAvatarClick}
         onEditProfile={handleEditProfileClick}
         onAddPlace={handleAddPlaceClick}
         onCardClick={handleCardClick}
       />
+
       <Footer />
 
       <PopupWithForm name="profile" title="Редактировать профиль" buttonText="Сохранить" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
@@ -55,6 +57,7 @@ function App() {
           <span id="description-error" className="popup__error"></span>
         </label>
       </PopupWithForm>
+
       <PopupWithForm name="card" title="Новое место" buttonText="Создать" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
         <label className="popup__field">
           <input id="place" className="popup__input popup__input_place_top" type="text" name="name" placeholder="Название" required minLength="2" maxLength="30"/>
@@ -65,6 +68,7 @@ function App() {
           <span id="link-error" className="popup__error"></span>
         </label>
       </PopupWithForm>
+
       <PopupWithForm name="avatar" title="Обновить аватар" buttonText="Сохранить" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
         <label className="popup__field">
           <input id="avatar-link" className="popup__input popup__input_place_bottom" type="url" name="avatar-link" placeholder="Ссылка на картинку" required/>
@@ -72,21 +76,8 @@ function App() {
         </label>
       </PopupWithForm>
 
-      <div className="popup popup_confirmation">
-        <div className="popup__container popup__container_confirmation">
-          <h2 className="popup__title popup__title_confirmation">Вы уверены?</h2>
-          <button className="popup__submit-button popup__submit-button_confirmation" type="submit">Да</button>
-          <button className="popup__close-button popup__close-button_confirmation" type="button"></button>
-        </div>
-      </div>
+      <PopupWithForm name="confirmation" title="Вы уверены?" buttonText="Да" />
 
-      {/* <div className="popup popup_photo">
-        <div className="popup__photo-container">
-          <img src="#" alt="#" className="popup__image"/>
-          <h3 className="popup__image-caption"></h3>
-          <button className="popup__close-button popup__close-button_photo" type="button"></button>
-        </div>
-      </div> */}
       <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
     </div>
   );
